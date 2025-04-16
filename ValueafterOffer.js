3549cf3b401e062c6c36a61653fbfde94c66138c
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2025-04-16 15:26:21 +0530

    Create ValueafterOffer.js

---
+++ ValueafterOffer.js
@@ -0,0 +1,11 @@
+let items=[250,64,500,300,900];
+let idx=0;
+
+
+for(let val of items){
+    console.log(`value at index ${idx}=${val}`);
+    let offer=val/10;
+    items[idx]=items[idx]-offer;
+    console.log(`value after offer = ${val}`);
+    idx++;
+}
