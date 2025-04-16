6f9dd3efa49b52e1f47fd46198d8aca3ba41e528
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2025-04-16 16:05:53 +0530

    Create ToString.js

---
+++ ToString.js
@@ -0,0 +1,15 @@
+
+let foodItems=["potato", "apple", "litchi", "tomato"];
+console.log(foodItems.toString());
+
+let marks=[97,90,78,89];
+console.log(marks.toString());
+
+
+// concatination
+
+let marvelHeros=["thor", "spiderman", "heros"];
+let dcHeros=["superman", "batman"];
+
+let heros=marvelHeros.concat(dcHeros);
+console.log(heros);
