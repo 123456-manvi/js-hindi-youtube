a7d087fa4897024682d417f6eecefd87fb1ac977
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2025-04-19 23:51:00 +0530

    Create useState-useEffect.js

---
+++ useState-useEffect.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+
+const Timer=()=>{
+const[seconds,setSeconds]=useState(0);
+
+useEffect(()=>{
+const interval=setInterval(()=>{
+setSeconds(prev=>prev+1);
+},1000);
+
+return ()=> clearInterval(interval);
+},[])
+
+rerurn(
+<div>
+<h3>timer: {seconds} </h3}
+</div>
+
+)}
+
+export default Timer;
